edd4a99eee72fdfa584b9c8927f8425d0e95acf2
Lucchetto <lucchetto.tie@live.com>
2026-05-30 16:27:51 +0200

    Version 1.0.2 (3)
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.zhenxiang.langctrl"
         minSdk = 33
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
